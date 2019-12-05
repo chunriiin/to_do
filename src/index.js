6eae28ba32b2b93e0d75cffa3277374bde695be3
岸田舞 <mai@metaphase.co.jp>
2019-12-05 21:48:14 +0900

    Use redux in todo app

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App.js";
-ReactDOM.render(<App />, document.getElementById("root"));
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers'
+import App from './components/App'
+
+const store = createStore(rootReducer)
+
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
